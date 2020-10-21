13cf750cd3f42cd98922f943a6ec1635b70b9954
David Benshimol <davidbs123@gmail.com>
2020-10-21 13:59:14 +0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Open for jobs!</p>
+        <p>Welcome - Open for Data Engineer jobs!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
